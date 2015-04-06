ad20a2fa9db4e4dafc29fe1e80cffed3d8471ec3
kimnorthcutt <kimnorthcutt@msn.com>
2015-04-06 16:22:30 -0500

    moved method out of a loop, changed UX

--- bj1.rb
+++ bj1.rb
@@ -1,3 +1,13 @@
+def check_card(card)
+  if card[0] == "A"
+   11
+  elsif card[0].to_i != 0
+    card[0].to_i
+  elsif card[0].to_i == 0
+    10
+  end
+end
+
 begin
   SUITS = ["Diamonds", "Clubs", "Spades", "Hearts"]
   VALUE = [2,3,4,5,6,7,8,9,10,"J","Q","K","A"]
@@ -5,16 +15,6 @@ begin
 
   system "clear"
   system "cls"
-
-  def check_card(card)
-    if card[0] == "A"
-      card[0].to_i + 11
-    elsif card[0].to_i != 0
-       card[0].to_i
-    elsif card[0].to_i == 0
-       card[0].to_i + 10
-    end
-  end
 
   player_bank = []
   player_first_card = deck.pop
@@ -30,13 +30,13 @@ begin
   computer_total = check_card(computer_first_card)
   puts "The computer has #{computer_first_card.join(" of ")} and the total is #{computer_total}"
   begin
-    puts "Would you like another card? Enter Yes or No?"
+    puts "Would you like another card? Enter y for yes or n for no?"
     response = gets.chomp.downcase
     if response == "y"
       extra_card = deck.pop
       player_bank << extra_card
       player_total += check_card(extra_card)
-      puts "You got #{extra_card.join(" of ")} and your new total is #{player_total}"
+      puts "Here is you hand #{player_bank}.You got #{extra_card.join(" of ")} and your new total is #{player_total}"
     elsif response == "n"
       puts "Ok. It's computer's turn."
     break
@@ -52,22 +52,21 @@ begin
       computer_extra_card = deck.pop
       computer_bank << computer_extra_card
       computer_total += check_card(computer_extra_card)
-      puts "Computer has #{computer_extra_card.join(" of ")} and its total is #{computer_total}"
+      puts "Computer's hand is #{computer_bank}. Computer has #{computer_extra_card.join(" of ")} and its total is #{computer_total}"
     end until (computer_total <= 21) && (computer_total >= 17) || computer_total > 21
   end
  
   if computer_total == player_total
     puts " We have a tie! I guess no one wins."
-    elsif ((computer_total <= 21) && (player_total > 21)) || ((computer_total <= 21) && (player_total <= 21) && (computer_total > player_total))
+  elsif ((computer_total <= 21) && (player_total > 21)) || ((computer_total <= 21) && (player_total <= 21) && (computer_total > player_total))
     puts "Computer Wins!"
-    elsif ((computer_total > 21 ) && (player_total <= 21)) || ((computer_total <= 21) && (player_total <= 21) && (player_total > computer_total))
+  elsif ((computer_total > 21 ) && (player_total <= 21)) || ((computer_total <= 21) && (player_total <= 21) && (player_total > computer_total))
     puts "You win!"
   end
 
-  puts "Would you like to play again"
+  puts "Would you like to play again? Please enter y for yes and n for no."
   player_answer = gets.chomp.downcase
-  if player_answer == "y"
-  elsif player_answer == "n"
+  if player_answer == "n"
     puts "Great game friend."
   end
 end while (player_answer == "y")
